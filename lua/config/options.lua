418f0ed30eb60838443c79c3de7e057149b0fae8
yongnianliu <lyn0328@qq.com>
2025-08-04 17:28:28 +0800

    feat(config):Add <C-H> => Right insert mode.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true, silent = true })
-vim.g.root_spec = { "lsp", { ".git", "lua", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "pom.xml" }, "cwd" }
+-- vim.api.nvim_set_keymap("i", "<C-H>", "<Left>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("i", "<C-J>", "<Down>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("i", "<C-K>", "<Up>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-L>", "<Right>", { noremap = true, silent = true })
+vim.g.root_spec =
+  { "lsp", { ".git", "lua", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "pom.xml" }, "cwd" }
