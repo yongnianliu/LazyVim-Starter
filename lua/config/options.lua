fc6ae63bd3b1ede73df007545edbeb866db9ec77
yongnianliu <lyn0328@qq.com>
2025-08-05 00:25:46 +0800

    feat(config):add nerd font config.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.guifont = "JetBrainsMono Nerd Font"
 vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("i", "<C-H>", "<Left>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("i", "<C-J>", "<Down>", { noremap = true, silent = true })
