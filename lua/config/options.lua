28fba91374c491bbb9743601671a76955d172b25
yongnianliu <lyn0328@qq.com>
2025-08-15 15:57:55 +0800

    feat(option): add clipboard support.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+if vim.loop.os_uname().sysname == "Darwin" then
+elseif vim.fn.has("linux") then
+else
+  -- Set a compatible clipboard manager
+  vim.g.clipboard = {
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+  }
+end
 vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("i", "<C-H>", "<Left>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("i", "<C-J>", "<Down>", { noremap = true, silent = true })
